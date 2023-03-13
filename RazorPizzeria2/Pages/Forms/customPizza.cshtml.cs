65ba7fe94829dac9bf595bfa8ba2ea4a669de886
Jarrod Thompson <jarrodthompson@Jarrods-MacBook-Pro.local>
2023-03-12 23:49:13 -0400

    First commit

---
+++ RazorPizzeria2/Pages/Forms/customPizza.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria2.Models;
+
+namespace RazorPizzeria2.Pages.Forms
+{
+	public class customPizzaModel : PageModel
+    {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
